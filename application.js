cca701026380051981b41499c2a9dc5479759723
JT Grauke <jtgrauke@gmail.com>
2013-08-30 16:38:39 -0700

    added description, earnings, and employment

--- application.js
+++ application.js
@@ -6,41 +6,81 @@ config = {
 
 // Packets of Career Coach data
 packet = {
-    earnings: 'occupationearnings'
+    occ_description: 'occupationdescription',
+    occ_earnings: 'occupationearnings',
+    occ_employment: 'occupationemployment',
+    occ_employmentSuccinct: 'occupationemploymentsuccinct'
 }
 
 // Object for the occupation you want data on
 occupation = {
-    name: 'Registered Nurses',
-    soc: '29-1141.00',
+    name: 'Computer Programers',
+    soc: '15-1131.00',
     zip: '83843',
     radius: '50'
 };
 
-function loadOccupation() {
-    // Pull in the data from Career Coach
+var url_data = {'OccupationId':occupation.soc, 'x-ResponseType':'json', 'x-Password':config.password};
+
+function occDescription() {
+    // Pull in the description of the occupation
     $.ajax({
         type: "GET",
-        url: config.baseUrl + "/packets/"+ packet.earnings +"/",
-        data: {'OccupationId':occupation.soc, 'x-ResponseType':'json', 'x-Password':config.password},
+        url: config.baseUrl + "/packets/" + packet.occ_description + "/",
+        data: url_data,
         beforeSend: function() {
-            $('.container').text('Loading...');
+            $('.container').hide();
         },
+        complete: function() {
+            $('.container').fadeIn(1000);
+        },
+    // Upon success - log the occupation description, education level, and entry requirements
+    }).success(function(data){
+        console.log(data);
+        var name = data['Name'],
+            description = data['Description'],
+            edLevel = data['EdLevel'],
+            entryRequirements = data['EntryRequirements'];
+        $('.occ_name').append(name);
+        $('.occ_desc').append(description);
+    });
+} occDescription();
+
+function occEarnings() {
+    // Pull in the earnings data for the occupation
+    $.ajax({
+        type: "GET",
+        url: config.baseUrl + "/packets/" + packet.occ_earnings + "/",
+        data: url_data,
+    // Upon success - log the data on wages
+    }).success(function(data){
+        console.log(data);
+        var ten = data['10'],
+            twenty_five = data['25'],
+            fifty = data['50'],
+            seventy_five = data['75'],
+            ninety = data['90'];
+        $('.occ_earn').html('<span class="data">'+ occupation.name +'</span> make about <span class="data">$' + fifty + '</span> an hour in your area.');
+    });
+} occEarnings();
+
+function occEmployment() {
+    // Pull in the employment data on the occupation
+    $.ajax({
+        type: "GET",
+        url: config.baseUrl + "/packets/" + packet.occ_employment + "/",
+        data: url_data,
     // Upon success - log the data
     }).success(function(data){
         console.log(data);
-        var tenth_percentile = data['10'],
-            twentyfifth_percentile = data['25'],
-            fiftieth_percentile = data['50'],
-            seventyfifth_percentile = data['75'],
-            ninetieth_percentile = data['90'];
-        $('.container').text("The median hourly wage for " + occupation.name + " is $" + fiftieth_percentile + ".");
+        var annOpen = data['AnnualOpenings'],
+            curEmp = data['CurrentEmployment'],
+            empTrend = data['EmploymentTrend'];
     });
-}
+} occEmployment();
 
 $(document).ready(function(){
 
-    // Load those occs
-    loadOccupation();
+
 
 });
